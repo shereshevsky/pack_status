32612aa39c27c97c20e749cdc00c5f3db5b42bdf
Alexander <shereshevsky@gmail.com>
2013-04-27 02:20:51 +0300

    .

--- get_status_functions.php
+++ get_status_functions.php
@@ -40,7 +40,6 @@ function fn_save_mail($itemcode, $email) {
 		'email' => $email,
 		'tr_number' => $itemcode
 	);
-	//$db->num_rows("INSERT INTO requests(email,tr_number) VALUES(?,?)", $email, $itemcode); 
 	$db->Insert($insertData, 'requests'); 
 }
 
@@ -50,11 +49,6 @@ function fn_send_mail($itemcode, $email, $txt) {
 
 function fn_periodic_check() {
 	global $db;
-	
-	//$five_days_ago = date('Y-m-d', time() - (5 * 24 * 60 * 60));
-	//$db->num_rows('DELETE FROM requests WHERE added < ?',$five_days_ago); 
-
-	//$results = $db->query("SELECT tr_number, email  FROM requests LIMIT ?",100);
 	$results = $db->ExecuteSQL('SELECT tr_number, email  FROM requests');
 	foreach ($results as $request) {
 		fn_israpost($request['tr_number'], $request['email'], true);
@@ -63,7 +57,6 @@ function fn_periodic_check() {
 
 function fn_delete_request($itemcode, $email) {
 	global $db;
-
 	$db->Delete("requests", "email = '$email' AND tr_number = '$itemcode'"); 
 }
 
