58b69f0ed32fc872af0152a78083388d7d3ad072
Alexander <shereshevsky@gmail.com>
2013-04-26 23:36:17 +0300

    .

--- get_status_functions.php
+++ get_status_functions.php
@@ -49,9 +49,10 @@ function fn_send_mail($itemcode, $email, $txt) {
 
 function fn_periodic_check() {
 	global $db;
-	//$sql = "delete from requests where added < DATE_SUB(NOW(),INTERVAL 5 DAY)";
-	$db->where('added', '< DATE_SUB(NOW(),INTERVAL 5 DAY)');
-	$db->delete('requests');
+	
+	$sql = "delete from requests where added < DATE_SUB(NOW(),INTERVAL 5 DAY)";
+	$db->rawQuery($sql);
+
 	$sql = "select tr_number, email from requests";
 	$results = $db->query($sql);
 	foreach ($results as $request) {
