16a47c57a9dbdacf548fdca6c77a50d01153d684
Alexander <shereshevsky@gmail.com>
2013-04-27 03:03:19 +0300

    mailto

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
             <ul class="nav">
               <li class="active"><a href="#">Home</a></li>
               <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li><a href="mailto:shereshevsky@gmail.com">Contact</a></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div>
